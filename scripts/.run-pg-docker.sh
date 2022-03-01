ca05a6f8309221103ced8faf7bd5e0cda037aaea
matheusdoedev <matheusdoe.dev@gmail.com>
2022-03-01 12:29:40 -0300

    feat: initial commit

---
+++ scripts/.run-pg-docker.sh
@@ -0,0 +1,2 @@
+docker run --rm --name pg -e POSTGRES_PASSWORD=docker -d -p 5432:5432 postgres &&
+docker run -p 8080:8080 -d -e PGADMIN_DEFAULT_EMAIL=admin@admin.com -e PGADMIN_DEFAULT_PASSWORD=docker -e PGADMIN_LISTEN_PORT=8080 --rm --name pgadmin dpage/pgadmin4
