e4e42c522137fb0bce99152693f7588782e0f58d
Matheus do É Santos <matheusdoe.dev@gmail.com>
2022-08-07 12:00:50 -0300

    chore: scripts and vscode settings updated

--- scripts/.npm-libs.sh
+++ scripts/.npm-libs.sh
@@ -1,1 +1,1 @@
-npm i -g @angular/cli @nestjs/cli @vue/cli eslint expo-cli ts-node-dev ts-node typescript yarn
+npm i -g @angular/cli @nestjs/cli @vue/cli eslint expo-cli ts-node-dev ts-node typescript typescript-language-server yarn
