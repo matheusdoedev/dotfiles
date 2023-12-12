90ca2e5f463cbfd44cabed2e24e9489d2f8d0202
Matheus do É Santos <58111490+matheusdoedev@users.noreply.github.com>
2023-12-12 14:12:24 -0300

    feat: update mysql docker script

--- run-mysql-docker.sh
+++ run-mysql-docker.sh
@@ -1,2 +1,3 @@
 #!/bin/zsh
-docker run --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=docker -e MYSQL_PASSWORD=docker -e MYSQL_USER=docker -d mysql:latest
+
+docker run --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=docker-d mysql:latest
